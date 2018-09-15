48ce92eba589f009638fa61407f89c28e81fbbdb
Sendhil Vel <myemail.msamvel@gmail.com>
2018-09-16 03:03:09 +0530

    This is main files
    This is main files

---
+++ dpackage.go
@@ -0,0 +1,11 @@
+package dmpackage
+
+/*import "fmt"
+
+func init() {
+	fmt.Println("This is demo package function init")
+}*/
+func TestFunc() string {
+	//fmt.Println("This is demo package function test")
+	return "test"
+}
