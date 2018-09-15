4ab18ee34151d4e0653364c08a75b5c0a87b75e4
Sendhil Vel <myemail.msamvel@gmail.com>
2018-09-16 03:09:53 +0530

    Added init function
    Added init function

--- dpackage.go
+++ dpackage.go
@@ -1,11 +1,18 @@
 package dmpackage
 
-/*import "fmt"
+import "fmt"
 
+/*
+	init : this is the function for init
+*/
 func init() {
 	fmt.Println("This is demo package function init")
-}*/
+}
+
+/*TestFunc : this is test function which return
+a string
+*/
 func TestFunc() string {
-	//fmt.Println("This is demo package function test")
+	fmt.Println("This is demo package function test")
 	return "test"
 }
